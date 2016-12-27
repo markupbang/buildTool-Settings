fe775322cc3e272109e8e0f2df734e6e42fb3e3b
bang so hyun <css@markupbang.com>
2016-12-27 14:44:25 +0900

    sorucemap 추가. 번들링 타겟파일이 여러개일 경우 타겟파일의 위치를 알 수 있음.

--- _gulp.sass.watch/gulpfile.js
+++ _gulp.sass.watch/gulpfile.js
@@ -2,13 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 var src = './src/sass/*.s+(a|c)ss'; // input file
 var dist = './src/css'; // output file
+var maps = './'; // map file
 
 gulp.task('sass', function () {
   return gulp.src(src)
+	.pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+	.pipe(sourcemaps.write(maps))
     .pipe(gulp.dest(dist));
 });
 
